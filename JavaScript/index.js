133af3a400a6c9962502ff7df4d1115db7f66ccc
Shikhar Kumar <shikharkumar@ymail.com>
2025-02-17 17:18:45 -0800

    Hero Section Created

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,33 @@
-//----------------------------------------- Work Experience -----------------------------------------
+//----------------------------------------- Hero -----------------------------------------
+const typingAnimationElement = document.getElementById('typing-animation');
 
+// Create an array of typing text
+const typingTexts = [
+'Shikhar Kumar.  ',
+'a Software Developer.  ',
+];
+
+// Create a function to display the typing animation for a given text
+function playTypingAnimation(text) {
+// Loop through each character and add it to the element
+for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+    typingAnimationElement.textContent += text[i];
+    }, i * 100); // Increase the delay to slow down the typing animation
+}
+
+// Once the animation is complete, reset the text and start over
+setTimeout(() => {
+    typingAnimationElement.textContent = '';
+    playTypingAnimation(typingTexts[(typingTexts.indexOf(text) + 1) % typingTexts.length]);
+}, text.length * 200);
+}
+
+// Start the typing animation loop
+playTypingAnimation(typingTexts[0]);
+
+
+//----------------------------------------- Work Experience -----------------------------------------
 // Makes Work Show Details Button Responsive
 document.querySelector('.work-btn').addEventListener("click", function() {
     if (this.textContent.trim() === "Show Details") {
@@ -25,3 +53,4 @@ course_btn.forEach(function(button) {
         }
     });
 });
+

c407e08f224ff78ff7ce8c9d1dc86f0331b9c590
Shikhar Kumar <shikharkumar@ymail.com>
2025-02-17 19:30:05 -0800

    Removed Typing Animation

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,32 +1,3 @@
-//----------------------------------------- Hero -----------------------------------------
-const typingAnimationElement = document.getElementById('typing-animation');
-
-// Create an array of typing text
-const typingTexts = [
-'Shikhar Kumar  ',
-'a Software Engineer  ',
-];
-
-// Create a function to display the typing animation for a given text
-function playTypingAnimation(text) {
-// Loop through each character and add it to the element
-for (let i = 0; i < text.length; i++) {
-    setTimeout(() => {
-    typingAnimationElement.textContent += text[i];
-    }, i * 100); // Increase the delay to slow down the typing animation
-}
-
-// Once the animation is complete, reset the text and start over
-setTimeout(() => {
-    typingAnimationElement.textContent = '';
-    playTypingAnimation(typingTexts[(typingTexts.indexOf(text) + 1) % typingTexts.length]);
-}, text.length * 200);
-}
-
-// Start the typing animation loop
-playTypingAnimation(typingTexts[0]);
-
-
 //----------------------------------------- Work Experience -----------------------------------------
 // Makes Work Show Details Button Responsive
 document.querySelector('.work-btn').addEventListener("click", function() {

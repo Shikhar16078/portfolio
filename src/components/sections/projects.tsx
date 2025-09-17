759f1b55dfadf27ff1be25331d78c8a1ed950183
Shikhar Kumar <shikharkumar78@gmail.com>
2025-09-17 17:20:32 +0000

    I added a new color called hero. Which should be the bg of hero section.

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -42,7 +42,7 @@ export default function ProjectsSection() {
               return (
                 <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                   <div className="p-1 h-full">
-                    <Card className="flex flex-col h-full bg-background/80">
+                    <Card className="flex flex-col h-full bg-background">
                       <CardHeader>
                         {projectImage && (
                           <Image
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ export default {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        hero: 'hsl(var(--hero))',
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',

5a7aaa5a88ea798bc6b7d82c75aa00a284ddf676
Shikhar Kumar <shikharkumar78@gmail.com>
2025-09-17 15:12:15 +0000

    remove the navbar entirely from website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/header';
 import HomeSection from '@/components/sections/home';
 import ExperienceSection from '@/components/sections/experience';
 import EducationSection from '@/components/sections/education';
@@ -8,8 +7,7 @@ import ContactSection from '@/components/sections/contact';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      <Header />
-      <main className="flex-1 w-full">
+      <main className="flex-1">
         <HomeSection />
         <ExperienceSection />
         <EducationSection />

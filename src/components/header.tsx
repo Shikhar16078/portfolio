3cc5f4705a478a4094026e7d641c1e89a9a275cc
Shikhar Kumar <shikharkumar78@gmail.com>
2025-09-17 16:33:45 +0000

    add glass effect in navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -70,7 +70,7 @@ export default function Header() {
   );
 
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center bg-background border-b sticky top-0 z-50">
+    <header className="px-4 lg:px-6 h-14 flex items-center bg-background/80 backdrop-blur-sm border-b sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between px-4 md:px-6 max-w-7xl">
         <Dialog>
           <DialogTrigger asChild>

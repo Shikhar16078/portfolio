5eabe69ddbb2f6e8ce48a111f2c19d846fc0dce1
Shikhar Kumar <shikharkumar78@gmail.com>
2025-09-17 14:52:29 +0000

    website content should be in center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import ContactSection from '@/components/sections/contact';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-[100dvh]">
+    <div className="flex flex-col min-h-[100dvh] items-center">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 w-full">
         <HomeSection />
         <ExperienceSection />
         <EducationSection />

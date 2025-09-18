7e700a11550bbcafdee49808cf49999f261ffca5
Shikhar Kumar <shikharkumar78@gmail.com>
2025-09-18 05:53:14 +0000

    you broke my work, my css is not loading :(

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-const repo = '/portfolio'; // your repo name with a leading slash
+const repo = '/portfolio';
 
 const nextConfig = {
   output: 'export',
   basePath: isProd ? repo : '',
-  assetPrefix: isProd ? `${repo}/` : undefined, // <-- add this
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  assetPrefix: isProd ? `${repo}/` : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -16,6 +14,12 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
       { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
     ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
